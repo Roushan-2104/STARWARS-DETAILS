2beb839254e680fa73d8eb44bf89f4a5b8897d69
Roushan Sharma <87894817+Roushan-2104@users.noreply.github.com>
2021-09-07 08:15:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let btn = document.getElementById("btn").addEventListener("click", (e)=>{
     e.preventDefault();
     let gender = document.getElementById("gender")
@@ -8,13 +7,13 @@ let btn = document.getElementById("btn").addEventListener("click", (e)=>{
     name.innerHTML = "<i>Loading...</i>";
     gender.innerHTML = "<i>Loading...</i>";
     img.innerHTML = "<i>Loading...</i>";
-    fetch("https://akabab.github.io/starwars-api/api/id/"+random_num+".json")
+    fetch(`https://akabab.github.io/starwars-api/api/id/${random_num}.json`)
     .then(response => response.json())
     .then(data =>{
         setTimeout(() => {
             name.innerText = data["name"];
             gender.innerText = data["gender"];
-            img.innerHTML = "<img src = '"+data["image"]+"' width = 100% height = 100%/>"
+            img.innerHTML = `<img src = ${data["image"]} width = 100% height = 100%/>`
         }, 500);
         
     })
